2303faa597f6fb52bb215fa727f883e3fc3ec3f9
Matheus Dario <dario.matheus97@gmail.com>
2022-05-02 12:01:11 -0300

    Seller entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,19 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.Connection;
+import java.util.Date;
+
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        System.out.println(seller);
     }
 }
